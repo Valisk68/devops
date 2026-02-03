87922dc8e07c4a223cdba798fff14cdfc06c2020
“valisk68” <shaikvalims@gmail.com>
2026-02-03 20:55:10 +0530

    arguments passed variable

---
+++ arg-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON2: Hello $PERSON1! How are you today?"
+echo "$PERSON1: Hi $PERSON2! I'm doing well, thank you. How about you?"
+echo "$PERSON2: I'm great, thanks for asking! Have you finished the DevOps course?"
+echo "$PERSON1: Yes, I completed it last week. It was really informative."
+echo "$PERSON2: That's awesome! What was your favorite part of the course?"
+echo "$PERSON1: I really enjoyed learning about CI/CD pipelines"
+echo "$PERSON2: I agree! Automation is a game-changer in DevOps."
+
