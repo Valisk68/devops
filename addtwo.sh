ad05256ebbb524826b7c88f33ab3ab83870a8dd6
“valisk68” <shaikvalims@gmail.com>
2026-02-03 21:11:44 +0530

    arguments passed variable

--- addtwo.sh
+++ addtwo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#add two numbers
-echo "Enter first number: "
-read num1
-echo "Enter second number: "
-read num2
-sum=$((num1 + num2))
-echo "The sum of $num1 and $num2 is: $sum"
+# #add two numbers
+# echo "Enter first number: "
+# read num1
+# echo "Enter second number: "
+# read num2
+# sum=$((num1 + num2))
+# echo "The sum of $num1 and $num2 is: $sum"
+
+x=$1
+y=$2
+z=$(x-y)
+echo "the value is: $z"
