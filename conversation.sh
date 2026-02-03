3cd59d95ae91cf1577cf2ec5b4b30150ca3343d6
“valisk68” <shaikvalims@gmail.com>
2026-02-03 20:15:22 +0530

    upated conversoin

---
+++ conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Raju: Hello Mastanvali! How are you today?"
+echo "Mastan: Hi Raju! I'm doing well, thank you. How about you?"
+echo "Raju: I'm great, thanks for asking! Have you finished the DevOps course?"
+echo "Mastan: Yes, I completed it last week. It was really informative."
+echo "Raju: That's awesome! What was your favorite part of the course?"
+echo "Mastan: I really enjoyed learning about CI/CD pipelines. It was fascinating to see how automation can streamline the development process."
+echo "Raju: I agree! Automation is a game-changer in DevOps. Did you get a chance to work on any projects?"
+
