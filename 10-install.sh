bab336bc48edc5084bfd2bd38c3177f8128b30cb
“valisk68” <shaikvalims@gmail.com>
2026-02-05 16:36:26 +0530

    with root error

--- 10-install.sh
+++ 10-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install nginx web server
-userid=($id -u)
+USERID=$(id -u)
 
-if [ $userid -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
     exit 1
 fi
